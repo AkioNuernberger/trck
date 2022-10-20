4c9ddf65deb80c08994762402c4e8fc9b674b066
AkioNuernberger <62014146+AkioNuernberger@users.noreply.github.com>
2022-10-20 22:18:59 +0200

    add python files

---
+++ main.py
@@ -0,0 +1,5 @@
+# get image from slack
+# define a variable called tgt_source with the URL & path of image from slack
+# create a while function which calls main to run thru every picture on source folder and call up matching.py 
+# break while function if there is a match 
+# return file name which included the actual name
